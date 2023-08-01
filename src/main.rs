134016f842c704cb5e24db7b41cedabc5aff9aaa
Maximilian Lehrbaum <maximilian.lehrbaum@k-business.com>
2023-08-01 16:01:49 +0200

    Update code and depedencies.

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,52 @@
-//! Blink the LED (connected to Pin PC 13) on and off with 1 second interval.
+//! Blinks an LED
+//!
+//! This assumes that a LED is connected to pc13 as is the case on the blue pill board.
+//!
+//! Note: Without additional hardware, PC13 should not be used to drive an LED, see page 5.1.2 of
+//! the reference manual for an explanation. This is not an issue on the blue pill.
 
-#![deny(unsafe_code)]  //  Don't allow unsafe code in this file.
-#![deny(warnings)]  //  If the Rust compiler generates a warning, stop the compilation with an error.
-#![no_main]  //  Don't use the Rust standard bootstrap. We will provide our own.
-#![no_std]  //  Don't use the Rust standard library. We are building a binary that can run on its own.
+#![deny(unsafe_code)]
+#![no_std]
+#![no_main]
 
-extern crate cortex_m;  //  Low-level functions for ARM Cortex-M3 processor in STM32 Blue Pill.
-#[macro_use]  //  Import macros from the following crates,
-extern crate cortex_m_rt;  //  Startup and runtime functions for ARM Cortex-M3.
-extern crate cortex_m_semihosting;  //  Debug console functions for ARM Cortex-M3.
-extern crate panic_semihosting;  //  Panic reporting functions, which transmit to the debug console.
-extern crate stm32f103xx_hal;  //  Hardware Abstraction Layer (HAL) for STM32 Blue Pill.
+use panic_halt as _;
 
-use core::fmt::Write;  //  Provides writeln() function for debug console output.
-use cortex_m_rt::ExceptionFrame;  //  Stack frame for exception handling.
-use cortex_m_semihosting::hio;  //  For displaying messages on the debug console.
-use stm32f103xx_hal::delay::Delay;  //  Delay timer.
-use stm32f103xx_hal::prelude::*;  //  Define HAL traits.
-use stm32f103xx_hal::stm32f103xx::Peripherals;  //  Clocks, flash memory, GPIO for the STM32 Blue Pill.
+use nb::block;
 
-//  Blue Pill starts execution at function main().
-entry!(main);
+use cortex_m_rt::entry;
+use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
 
-//  Blue Pill starts execution here. "-> !" means this function will never return (because of the loop).
+#[entry]
 fn main() -> ! {
-    //  Show "Hello, world!" on the debug console, which is shown in OpenOCD. "mut" means that this object is mutable, i.e. it can change.
-    let mut debug_out = hio::hstdout().unwrap();
-    writeln!(debug_out, "Hello, world!").unwrap();
+    // Get access to the core peripherals from the cortex-m crate
+    let cp = cortex_m::Peripherals::take().unwrap();
+    // Get access to the device specific peripherals from the peripheral access crate
+    let dp = pac::Peripherals::take().unwrap();
 
-    //  Get peripherals (clocks, flash memory, GPIO) for the STM32 Blue Pill microcontroller.
-    let bluepill = Peripherals::take().unwrap();
+    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
+    // HAL structs
+    let mut flash = dp.FLASH.constrain();
+    let rcc = dp.RCC.constrain();
 
-    //  Get the clocks from the STM32 Reset and Clock Control (RCC) and freeze the Flash Access Control Register (ACR).
-    let mut rcc = bluepill.RCC.constrain();
-    let mut flash = bluepill.FLASH.constrain();
+    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
+    // `clocks`
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    //  Get GPIO Port C, which also enables the Advanced Peripheral Bus 2 (APB2) clock for Port C.
-    let mut gpioc = bluepill.GPIOC.split(&mut rcc.apb2);
+    // Acquire the GPIOC peripheral
+    let mut gpioc = dp.GPIOC.split();
 
-    //  Use Pin PC 13 of the Blue Pill for GPIO Port C. Select Output Push/Pull mode for the pin, which is connected to our LED.
+    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
+    // in order to configure the port. For pins 0-7, crl should be passed instead.
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    // Configure the syst timer to trigger an update every second
+    let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
+    timer.start(1.Hz()).unwrap();
 
-    //  Create a delay timer from the RCC clocks.
-    let cp = cortex_m::Peripherals::take().unwrap();
-    let mut delay = Delay::new(cp.SYST, clocks);
-
-    //  Loop forever.
+    // Wait for the timer to trigger an update and change the state of the LED
     loop {
-        //  Output 3.3V on the LED Pin and show a message in OpenOCD console.
+        block!(timer.wait()).unwrap();
         led.set_high();
-        writeln!(debug_out, "LED is OFF!").unwrap();
-
-        //  Wait 1,000 millisec (1 sec).
-        delay.delay_ms(2_000_u16);
-
-        //  Output 0V on the LED Pin and show a message in OpenOCD console.
+        block!(timer.wait()).unwrap();
         led.set_low();
-        writeln!(debug_out, "LED is ON!").unwrap();
-
-        //  Wait 1,000 millisec (1 sec).
-        delay.delay_ms(1_000_u16);
     }
-}
-
-//  For any hard faults, show a message on the debug console and stop.
-exception!(HardFault, hard_fault);
-
-fn hard_fault(ef: &ExceptionFrame) -> ! {
-    panic!("Hard fault: {:#?}", ef);
-}
-
-//  For any unhandled interrupts, show a message on the debug console and stop.
-exception!(*, default_handler);
-
-fn default_handler(irqn: i16) {
-    panic!("Unhandled exception (IRQn = {})", irqn);
 }
